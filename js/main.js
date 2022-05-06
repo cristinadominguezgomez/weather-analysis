7f95f173ddcf32d8c0b992b6e83d4d57baf633d2
Shirley Gianina <shirley.cd853f@Shirleys-MacBook-Pro.local>
2022-05-06 05:16:58 -0500

    get data by console

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
-("use strict");
-
+"use strict";
 const button = document.querySelector("button");
-
 const getWeather = async (position) => {
   const { latitude, longitude } = position.coords;
-
-  const key = "e4bcc9331cb23fccb8f2334e39b3b0a4";
-
+  const key = "";
   try {
     const res = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,daily&appid=${key}`
     );
     if (res.ok) {
       const { hourly } = await res.json();
+      const datosHora = [];
+
       for (let i = 0; i < 8; i++) {
-        console.log(hourly[i]);
+        datosHora.push(hourly[i].weather);
+        const previsionHora = datosHora.map((hora) => {
+          return { main: hora[0].main };
+        });
+        switch (previsionHora[i].main) {
+          case "Rain":
+            console.log("Rain");
+            break;
+          case "Clouds":
+            console.log("Clouds");
+            break;
+          case "Clear":
+            console.log("Clear");
+            break;
+          default:
+            console.log("por defecto");
+        }
       }
     } else {
       console.log("Hubo un error en la petición");
@@ -23,7 +37,6 @@ const getWeather = async (position) => {
     console.error(error.message);
   }
 };
-
-button.addEventListener("click", (event) => {
+button.addEventListener("click", () => {
   navigator.geolocation.getCurrentPosition(getWeather);
 });
